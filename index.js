5c774edf79e9b7d7020eb31b1dad1c7da58c7ff4
Theresa Bailey <tbailey.nylaw@gmail.com>
2018-08-08 18:45:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 74
-var message = `is ${height} inches tall to be less than 40`
+var message = ` ${Susan} is ${height} inches tall to be less than 40`
 
 module.exports = {name, height, message}
