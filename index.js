e3664c1e1c86e5a5c97f049dca172f855a66d8a4
Theresa Bailey <tbailey.nylaw@gmail.com>
2018-08-08 18:43:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
+var name = "Susan"
 var height = 74
-var message = ` is ${height} inches tall`
-
+var message = ` ${name} is ${height} inches tall to be less than 40`
+if message = ` ${name} is ${height} inches tall to be less than 40` 'name' = "Joe"; else 'name' = "Susan"
 module.exports = {name, height, message}
